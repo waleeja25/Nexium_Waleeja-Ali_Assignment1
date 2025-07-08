9757ec343384d3cd55a5a6a06faf6e033d6eb8d3
Waleeja Ali <waleeja.ali25@gmail.com>
2025-07-08 21:21:46 +0500

    improved theme

--- quote-generator/app/page.tsx
+++ quote-generator/app/page.tsx
@@ -3,12 +3,15 @@ import { Label } from '@/components/ui/label';
 
 export default function HomePage() {
   return (
-    <div className="flex justify-center items-start pt-16 px-4">
-      <div className="w-full max-w-lg bg-[#0a1d37] p-14 rounded-xl shadow-lg">
-        <Label htmlFor='topic'  className="text-3xl font-bold mb-6 text-center text-white">Enter a topic to get inspired</Label>
+<div className="flex justify-center items-start pt-16 px-4">
+  <div className="w-full max-w-lg bg-[#0a1d37] p-14 rounded-2xl shadow-2xl ring-1 ring-cyan-600/40 ring-offset-2 ring-offset-[#0a1d37] transition duration-300">
+    <Label
+      htmlFor="topic"
+      className="block text-3xl font-extrabold mb-8 text-center text-white/90 drop-shadow-md tracking-wide">
+      Enter a topic to get inspired
+    </Label>
         <QuoteForm />
-      </div>
-
     </div>
+  </div>
   );
 }
